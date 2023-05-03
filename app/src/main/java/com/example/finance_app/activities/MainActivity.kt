f8e9b31f961225394619e390589cd6777dc3a15e
Nisal <it21259302@my.sliit.lk>
2023-05-03 11:16:25 +0530

    :memo: add insert data into fire base

---
+++ app/src/main/java/com/example/finance_app/activities/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.finance_app.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.finance_app.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnInsertData: Button
+    private lateinit var btnFetchData: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
+        btnInsertData = findViewById(R.id.btnInsertData)
+        btnFetchData = findViewById(R.id.btnFetchData)
+
+        btnInsertData.setOnClickListener {
+            val intent = Intent(this, InsertionActivity::class.java)
+            startActivity(intent)
+        }
+
+      /*  btnFetchData.setOnClickListener {
+            val intent = Intent(this, FetchingActivity::class.java)
+            startActivity(intent)
+        } */
+
+    }
+}
