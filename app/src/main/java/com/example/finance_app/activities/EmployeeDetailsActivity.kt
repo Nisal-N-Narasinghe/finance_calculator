714c7d11f01b2bb787c1559d75efaae838614920
Nisal <it21259302@my.sliit.lk>
2023-05-03 13:07:28 +0530

    :memo: Update data using a pop up dialog box

--- app/src/main/java/com/example/finance_app/activities/EmployeeDetailsActivity.kt
+++ app/src/main/java/com/example/finance_app/activities/EmployeeDetailsActivity.kt
@@ -1,10 +1,15 @@
 package com.example.finance_app.activities
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.finance_app.R
+import com.example.finance_app.models.EmployeeModel
+import com.google.firebase.database.FirebaseDatabase
 
 
 class EmployeeDetailsActivity: AppCompatActivity() {
@@ -22,6 +27,69 @@ class EmployeeDetailsActivity: AppCompatActivity() {
 
         initView()
         setValuesToViews()
+
+        btnUpdate.setOnClickListener {
+            openUpdateDialog(
+                intent.getStringExtra("empId").toString(),
+                intent.getStringExtra("empName").toString()
+            )
+        }
+    }
+
+    private fun openUpdateDialog(
+        empId: String,
+        empName: String
+    ) {
+        val mDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val mDialogView = inflater.inflate(R.layout.update_dialog, null)
+
+        mDialog.setView(mDialogView)
+
+        val etEmpName = mDialogView.findViewById<EditText>(R.id.etEmpName)
+        val etEmpAge = mDialogView.findViewById<EditText>(R.id.etEmpAge)
+        val etEmpSalary = mDialogView.findViewById<EditText>(R.id.etEmpSalary)
+
+        val btnUpdateData = mDialogView.findViewById<Button>(R.id.btnUpdateData)
+
+        etEmpName.setText(intent.getStringExtra("empName").toString())
+        etEmpAge.setText(intent.getStringExtra("empAge").toString())
+        etEmpSalary.setText(intent.getStringExtra("empSalary").toString())
+
+        mDialog.setTitle("Updating $empName Record")
+
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+
+        btnUpdateData.setOnClickListener {
+            updateEmpData(
+                empId,
+                etEmpName.text.toString(),
+                etEmpAge.text.toString(),
+                etEmpSalary.text.toString()
+            )
+
+            Toast.makeText(applicationContext, "Employee Data Updated", Toast.LENGTH_LONG).show()
+
+            //we are setting updated data to our textviews
+            tvEmpName.text = etEmpName.text.toString()
+            tvEmpAge.text = etEmpAge.text.toString()
+            tvEmpSalary.text = etEmpSalary.text.toString()
+
+            alertDialog.dismiss()
+        }
+    }
+
+    private fun updateEmpData(
+        id: String,
+        name: String,
+        age: String,
+        salary: String
+    ) {
+        val dbRef = FirebaseDatabase.getInstance().getReference("Employees").child(id)
+        val empInfo = EmployeeModel(id, name, age, salary)
+        dbRef.setValue(empInfo)
+
 
     }
 
